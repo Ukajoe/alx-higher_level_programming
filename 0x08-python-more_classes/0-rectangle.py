ca56d662a214f0d9c7bdd27897156526574af812
UKA JOSEPH <ukaukwauyojoseph@gmail.com>
2023-01-02 19:35:35 -0800

    Number

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python3
+# 0-rectangle.py
+# UKA JOSEPH <ukaukwauyojoseph@gmail.com>
+
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
