9192f04dfbe887b585d2695c0cff388374a2e4ce
UKA JOSEPH <ukaukwauyojoseph@gmail.com>
2023-01-09 09:28:53 -0800

    good

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python3
+"""MyList
+"""
+
+
+class MyList(list):
+    """Contains list
+    """
+
+    def print_sorted(self):
+        """Prints self in sorted format
+        """
+
+        print(sorted(self))
